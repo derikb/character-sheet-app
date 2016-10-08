8b79ac3b92a96e8f80b0c532dbfe1be02ab79cc2
Derik Badman <derik@springshare.com>
2016-10-08 12:29:48 -0400

    storage key prefix fixes #21

--- src/main.js
+++ src/main.js
@@ -531,12 +531,30 @@ const character_model = {
  */
 const Storage = {
 	/**
+	 * A prefix to attack to the random keys to differentiate them from any other storage for the current location/domain
+	 */
+	prefix: '',
+	/**
+	 * Set the prefix
+	 * @param {String} prefix string to prefix the randomly generated key
+	 */
+	setPrefix: function (prefix) {
+		this.prefix = prefix;
+	},
+	/**
 	 * Returns blank or the value for the key
 	 * @param {String} key
 	 * @return {String}
 	 */
 	get: function (key) {
-		const txt = localStorage.getItem(key);
+		let txt = localStorage.getItem(`${this.prefix}${key}`);
+		// backward compatibile cleanup later @todo
+		if (txt === null) {
+			txt = localStorage.getItem(key);
+			if (txt !== null) {
+				this.set(key, txt);
+			}
+		}
 		return (txt !== null) ? txt : '';
 	},
 	/**
@@ -548,7 +566,11 @@ const Storage = {
 	 */
 	set: function (key, txt) {
 		try {
-			localStorage.setItem(key, txt);
+			localStorage.setItem(`${this.prefix}${key}`, txt);
+			// backwards compatible cleanup for non prefixed saved characters @todo remove
+			if (localStorage.getItem(key) !== null) {
+				localStorage.removeItem(key);
+			}
 		} catch (e) {
 			// Should only happen when over quota
 			return false;
@@ -561,22 +583,32 @@ const Storage = {
 	 * @return void
 	 */
 	remove: function (key) {
-		localStorage.removeItem(key);
+		localStorage.removeItem(`${this.prefix}${key}`);
 	},
 	/**
-	 * Get an array of all keys
+	 * Get an array of all keys with the key prefix
 	 * @return {Array}
 	 */
 	getAllKeys: function () {
 		const keys = [];
 		if (localStorage.length > 0) {
 			for (let i = 0; i < localStorage.length; i++) {
-				keys.push(localStorage.key(i));
+				// limit to 
+				const key = localStorage.key(i);
+				// check for prefix
+				if (key.indexOf(this.prefix) !== 0) {
+					// backwards compatibility for a little while @todo remove
+					if (!key.match(/^[a-z0-9]{7}$/)) {
+						continue;
+					}
+				}
+				keys.push(key);
 			}
 		}
 		return keys;
 	}
 };
+Storage.setPrefix('charsheet-5e-');
 
 /**
  * If we save based on character name key then we have to account for name changes to change the localStorage key..
