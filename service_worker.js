8ce385988eed6ba1cb7e079ccef45e4c490d722a
Derik Badman <derik@springshare.com>
2016-10-16 17:35:16 -0400

    bump to 1.4.0

--- service_worker.js
+++ service_worker.js
@@ -9,7 +9,7 @@
  * Caching all the files we use except the service worker itself
  */
 var config = {
-	cacheName: 'static-v1.3.0',
+	cacheName: 'static-v1.4.0',
 	staticCacheItems: [
 		'/index.html',
 		'/main.min.js',
