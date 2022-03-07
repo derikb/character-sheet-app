7a7564d965549fd5718d75603fa9e1826476df3e
Simon De Ridder <simon.deridder+github@edpnet.be>
2022-03-07 21:39:55 +0100

    add writing of backgrounds

--- conversion_scripts/domain/backgrounds.py
+++ conversion_scripts/domain/backgrounds.py
@@ -1,8 +1,8 @@
-"""Defines the domain of race definitions."""
+"""Defines the domain of background definitions."""
 from __future__ import annotations
 
 from pydantic import Extra  # pylint: disable=unused-import
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from .books import Book
 
@@ -10,5 +10,5 @@ from .books import Book
 class Background(BaseModel, extra=Extra.forbid):
 	"""Definition of a background."""
 
-	name: str
-	source: Book
+	name: str = Field(description="Background name.")
+	source: Book = Field(description="Source book that defines the background.")
