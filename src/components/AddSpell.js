64c65696df9407f3911452432733c538d3a95442
NeptuneRjo <rjo9000@outlook.com>
2023-05-09 18:41:28 -0400

    cleaned up syntax and redundant code; added generic error handling for API

--- src/components/AddSpell.js
+++ src/components/AddSpell.js
@@ -1,31 +1,3 @@
-
-const template = document.createElement('template');
-template.innerHTML = `
-<link rel="stylesheet" href="./styles.css">
-<style>
-:host {
-    display: block;
-    padding: .5rem;
-    font-size: 1.125rem;
-    background-color: white;
-    display: flex;
-    align-items: baseline;
-    position: relative;
-    flex-wrap: wrap;
-}
-
-button {
-    font-size: inherit;
-    background-color: var(--primary-color, black);
-    color: white;
-    border: none;
-    border-radius: .5rem;
-    margin-left: 1rem;
-    flex: 0 1 auto;
-}
-</style>
-`;
-
 class AddSpell {
     constructor (el, cur_character, emitter) {
         this.el = el;
@@ -33,15 +5,19 @@ class AddSpell {
         this.emitter = emitter;
         this.el.addEventListener('click', this._openSpellModal.bind(this));
     };
+
     /**
      * @param {string} spellLevel
      * @returns array containing the index, name, and url
      */
-
     async getSpellData (spellLevel) {
         const apiUrl = `https://www.dnd5eapi.co/api/spells?level=${spellLevel}&school=illusion&school=abjuration&school=conjuration&school=divination&school=enchantment&school=evocation&school=necromancy&school=psionic&school=transmutation`;
         const response = await fetch(apiUrl, { method: 'GET' });
         const { results } = await response.json();
+
+        if (!response.ok) {
+            throw new Error('Error fetching API resource.');
+        };
 
         return results; // index, name, and url
     };
@@ -62,24 +38,32 @@ class AddSpell {
         const content = document.importNode(template.content, true);
         const list = content.querySelector('ul');
         const spellLevel = ev.target.dataset.level;
-        const spells = await this.getSpellData(spellLevel);
 
-        spells.forEach(spell => {
+        try {
+            const spells = await this.getSpellData(spellLevel);
+
+            spells.forEach(spell => {
+                const item = document.createElement('div');
+                const button = document.createElement('button');
+                const spellName = spell.name.replace(/ /g, '-').replace(/'/g, '\'');
+    
+                button.innerText = spell.name;
+                button.dataset.field = 'spells';
+                button.dataset.subfield = spellLevel;
+                button.dataset.name = spellName;
+                button.classList.add('btn', 'btn-plain');
+    
+                button.addEventListener('click', this._handleAddNewSpell.bind(this));
+    
+                item.appendChild(button);
+                list.appendChild(item);
+            });
+        } catch (error) {
+            console.log(error);
             const item = document.createElement('div');
-            const button = document.createElement('button');
-            const spellName = spell.name.replace(/ /g, '-').replace(/'/g, '\'');
-
-            button.innerText = spell.name;
-            button.dataset.field = 'spells';
-            button.dataset.subfield = spellLevel;
-            button.dataset.name = spellName;
-            button.classList.add('btn', 'btn-plain');
-
-            button.addEventListener('click', this._handleAddNewSpell.bind(this));
-
-            item.appendChild(button);
+            item.innerText = 'Unable to access this resource at the moment.';
             list.appendChild(item);
-        });
+        };
 
         this.spellDialog.setContent([...content.children]);
         this.spellDialog.open();
