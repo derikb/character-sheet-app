143929fa97a7c2e9a0224c03df9f17186d085910
NeptuneRjo <rjo9000@outlook.com>
2023-09-04 14:57:51 -0400

    Add more content to the spell item

--- src/components/SpellItem.js
+++ src/components/SpellItem.js
@@ -3,9 +3,43 @@ import { getCurrentCharacter } from '../services/CharacterService';
 
 const template = document.createElement('template');
 template.innerHTML = `
+<link rel="stylesheet" href="./styles.css">
 <style>
     .panel {
         display: none;
+        flex-direction: column;
+    }
+    .accordion {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 4px;
+    }
+    .accordion:hover {
+        cursor: pointer;
+        background-color: #f7f7f7;
+    }
+    .spell-item-name {
+        font-weight: bold;
+    }
+    .spell-item-add {
+        margin-bottom: 0;
+    }
+    .spell-item-desc {
+        padding: 4px;
+    }
+    .spell-item-hl {
+        padding: 4px;
+    }
+    .spell-item-stats {
+        list-style-type: none;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 4px;
+        padding: 4px;
+    }
+    p {
+        max-width: 72ch;
     }
 </style>
 <div class="accordion">
@@ -13,9 +47,15 @@ template.innerHTML = `
     <button class="spell-item-add">Add</button>
 </div>
 <div class="panel">
-    <p class="spell-item-desc"></p>
-    <p class="spell-item-hl"></p>
-    <span class="spell-item-range"></span>
+    <ul class="spell-item-stats">
+        <li class="spell-item-range"></li>
+        <li class="spell-item-comps"></li>
+        <li class="spell-item-dur"></li>
+        <li class="spell-item-casting"></li>
+        <li class="spell-item-conc"></li>
+    </ul>
+    <div class="spell-item-desc"></div>
+    <div class="spell-item-hl"></div>
 </div>
 `;
 
@@ -30,8 +70,9 @@ class SpellItem extends HTMLElement {
         this.accordion = this.shadowRoot.querySelector('.accordion');
         this.panel = this.shadowRoot.querySelector('.panel');
         this.add_button = this.shadowRoot.querySelector('.spell-item-add');
+        this.stats = this.shadowRoot.querySelector('.spell-item-stats');
     
-        this.accordion.addEventListener('click', this._handleAccordionClick);
+        this.accordion.addEventListener('click', this._handleAccordionClick.bind(this));
         this.add_button.addEventListener('click', this._handleAddNewSpell.bind(this));
 
         this.spell = spellData.filter((spell) => spell.name === this.dataset.name)[0]; // filter returns an array
@@ -43,6 +84,37 @@ class SpellItem extends HTMLElement {
     disconnectedCallback () {
         this.accordion.removeEventListener('click', this._handleAccordionClick);
         this.add_button.removeEventListener('click', this._handleAddNewSpell);
+    }
+
+    _handleAddNewSpell (ev) {
+        const level = this.dataset.subfield;
+
+        this.cur_character.setSpells(this.spell, level);
+    }
+
+    _handleAccordionClick () {
+        if (this.panel.style.display === 'flex') {
+            this.panel.style.display = 'none';
+        } else {
+            this.panel.style.display = 'flex';
+        }
+    }
+    /**
+     * Populate the content of the spell item
+     */
+    populateSpellItem () {
+        this.spellName = this.spell.name;
+        this.spellDesc = this.spell.desc;
+
+        this.spellComps = this.spell.components;
+        this.spellDur = this.spell.duration;
+        this.spellCasting = this.spell.casting_time;
+        this.spellRange = this.spell.range;
+        this.spellConcentration = this.spell.concentration;
+
+        if (this.spell.higher_level !== undefined) {
+            this.spellHl = this.spell.higher_level;
+        }
     }
     /**
      * Getter: Content of spell name
@@ -68,20 +140,14 @@ class SpellItem extends HTMLElement {
      * Setter: Content of spell's higher-level-casting
      */
     set spellHl (value) {
-        this.panel.querySelector('.spell-item-hl').innerHTML = value;
-    }
-    /**
-     * Getter: Content of spell range
-     * @returns {String}
-     */
-    get spellRange () {
-        return this.panel.querySelector('.spell-item-range').innerHTML;
-    }
-    /**
-     * Setter: Content of spell range
-     */
-    set spellRange (value) {
-        this.panel.querySelector('.spell-item-range').innerHTML = value;
+        this.panel.querySelector('.spell-item-hl').innerHTML = `<strong>At Higher Levels:</strong> `;
+
+        value.forEach((val) => {
+            const p = document.createElement('p');
+            p.innerHTML = val;
+
+            this.panel.querySelector('.spell-item-hl').appendChild(p);
+        });
     }
     /**
      * Getter: Content of spell description
@@ -94,39 +160,91 @@ class SpellItem extends HTMLElement {
      * Setter: Content of spell description
      */
     set spellDesc (value) {
-        this.panel.querySelector('.spell-item-desc').innerHTML = value;
-    }
+        value.forEach((val) => {
+            const p = document.createElement('p');
+            p.innerHTML = val;
 
-    _handleAddNewSpell (ev) {
-        const level = this.dataset.subfield;
-
-        this.cur_character.setSpells(this.spell, level);
-    }
-
-    _handleAccordionClick (ev) {
-        const target = ev.target.nextElementSibling;
-
-        if (target === null || !target.classList.contains('panel')) {
-            return;
-        }
-        
-        if (target.style.display === 'block') {
-            target.style.display = 'none';
-        } else {
-            target.style.display = 'block';
-        }
+            this.panel.querySelector('.spell-item-desc').appendChild(p);
+        });
     }
     /**
-     * Populate the content of the spell item
+     * Getter: Content of spell range
+     * @returns {String}
      */
-    populateSpellItem () {
-        this.spellName = this.spell.name;
-        this.spellRange = this.spell.range;
-        this.spellDesc = this.spell.desc;
+    get spellRange () {
+        return this.panel.querySelector('.spell-item-range').innerHTML;
+    }
+    /**
+     * Setter: Content of spell range
+     */
+    set spellRange (value) {
+        value = `<strong>Range:</strong> ${value}`;
 
-        if (this.spell.higher_level !== undefined) {
-            this.spellHl = this.spell.higher_level;
-        }
+        this.panel.querySelector('.spell-item-range').innerHTML = value;
+    }
+    /**
+     * Getter: Content of the spell's components
+     * @returns {String}
+     */
+    get spellComps () {
+        return this.stats.querySelector('.spell-item-comps').innerHTML;
+    }
+    /**
+     * Setter: Content of the spell's components
+     */
+    set spellComps (value) {
+        this.stats.querySelector('.spell-item-comps').innerHTML = `<strong>Components:</strong> `;
+
+        value.forEach((val) => {
+            const span = document.createElement('span');
+            span.innerHTML = val;
+
+            this.stats.querySelector('.spell-item-comps').appendChild(span);
+        });
+    }
+    /**
+     * Getter: Content of the spell duration
+     * @returns {String}
+     */
+    get spellDur () {
+        return this.stats.querySelector('.spell-item-dur').innerHTML;
+    }
+    /**
+     * Setter: Content of the spell duration
+     */
+    set spellDur (value) {
+        value = `<strong>Duration:</strong> ${value}`;
+
+        this.stats.querySelector('.spell-item-dur').innerHTML = value;
+    }
+    /**
+     * Getter: Content of the spell casting time
+     * @returns {String}
+     */
+    get spellCasting () {
+        return this.stats.querySelector('.spell-item-casting').innerHTML;
+    }
+    /**
+     * Setter: Content of the spell casting time
+     */
+    set spellCasting (value) {
+        value = `<strong>Casting time:</strong> ${value}`;
+
+        this.stats.querySelector('.spell-item-casting').innerHTML = value;
+    }
+    /**
+     * Getter: Content of the spell concentration
+     */
+    get spellConcentration () {
+        return this.stats.querySelector('.spell-item-conc').innerHTML;
+    }
+    /**
+     * Setter: Content of the spell concentration
+     */
+    set spellConcentration (value) {
+        value = `<strong>Concentration:</strong> ${value}`;
+
+        this.stats.querySelector('.spell-item-conc').innerHTML = value;
     }
 }
 
