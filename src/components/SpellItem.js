c2606435ba27b9d9c257310e8b7fcae6cf91e945
NeptuneRjo <rjo9000@outlook.com>
2024-01-13 01:29:15 -0500

    Created utility methods to cleanup and declutter the spellitem class

--- src/components/SpellItem.js
+++ src/components/SpellItem.js
@@ -75,31 +75,77 @@ class SpellItem extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.#setupElements();
     }
 
     connectedCallback () {
-        this.accordion = this.shadowRoot.querySelector('.accordion');
-        this.accordion_content = this.shadowRoot.querySelector('.accordion-content');
-        this.accordion_icon = this.shadowRoot.querySelector('.accordion-icon');
+        this.#loadData();
+        this.#populateSpellItem();
 
-        this.panel = this.shadowRoot.querySelector('.panel');
-        this.add_button = this.shadowRoot.querySelector('.spell-item-add');
-        this.stats = this.shadowRoot.querySelector('.spell-item-stats');
-    
-        this.accordion.addEventListener('click', this._handleAccordionClick.bind(this));
-        this.add_button.addEventListener('click', this._handleAddNewSpell.bind(this));
-
-        this.spell = spellData.filter((spell) => spell.name === this.dataset.name)[0]; // filter returns an array
-        this.cur_character = getCurrentCharacter();
-        
-        this.populateSpellItem();
-
-        this.accordion_icon.innerHTML = '&#8964;';
+        this.#setupEventListeners();
     }
 
     disconnectedCallback () {
-        this.accordion.removeEventListener('click', this._handleAccordionClick);
-        this.add_button.removeEventListener('click', this._handleAddNewSpell);
+        this.#removeEventListeners();
+    }
+
+    // Group together html elements for readability
+    #setupElements () {
+        this.elements = {
+            accordion: this.shadowRoot.querySelector('.accordion'),
+            accordion_icon: this.shadowRoot.querySelector('.accordion-icon'),
+            panel: this.shadowRoot.querySelector('.panel'),
+            add_button: this.shadowRoot.querySelector('.spell-item-add'),
+            stats: this.shadowRoot.querySelector('.spell-item-stats'),
+            spell_range: this.shadowRoot.querySelector('.spell-item-range'),
+            spell_components: this.shadowRoot.querySelector('.spell-item-comps'),
+            spell_duration: this.shadowRoot.querySelector('.spell-item-dur'),
+            spell_casting: this.shadowRoot.querySelector('.spell-item-casting'),
+            spell_concentration: this.shadowRoot.querySelector('.spell-item-conc'),
+            spell_name: this.shadowRoot.querySelector('.spell-item-name'),
+            spell_description: this.shadowRoot.querySelector('.spell-item-desc'),
+            spell_higher_level: this.shadowRoot.querySelector('.spell-item-hl')
+        };
+    }
+
+    #setupEventListeners () {
+        this.elements.accordion.addEventListener('click', this._handleAccordionClick.bind(this));
+        this.elements.add_button.addEventListener('click', this._handleAddNewSpell.bind(this));
+    }
+
+    #removeEventListeners () {
+        this.elements.accordion.removeEventListener('click', this._handleAccordionClick);
+        this.elements.add_button.removeEventListener('click', this._handleAddNewSpell);
+    }
+    
+    #loadData () {
+        this.cur_character = getCurrentCharacter();
+        this.spell = spellData.filter((spell) => spell.name === this.dataset.name)[0]; // filter returns an array
+        this.elements.accordion_icon.innerHTML = '&#8964;'; // down-chevron
+    }
+
+    // Declutters #populateSpellItem
+    #setElementContent (selector, content) {
+        this.elements[selector].innerHTML = content;
+    }
+    
+    /**
+     * Populate the content of the spell item
+     */
+    #populateSpellItem () {
+        const spell = this.spell;
+
+        this.#setElementContent('spell_name', spell.name);
+        this.#setElementContent('spell_description', spell.desc);
+        this.#setElementContent('spell_components', spell.components);
+        this.#setElementContent('spell_duration', spell.duration);
+        this.#setElementContent('spell_casting', spell.casting_time);
+        this.#setElementContent('spell_range', spell.range);
+        this.#setElementContent('spell_concentration', spell.concentration);
+
+        if (this.spell.higher_level !== undefined) {
+            this.#setElementContent('spell_higher_level', spell.higher_level);
+        }
     }
 
     _handleAddNewSpell (ev) {
@@ -109,164 +155,15 @@ class SpellItem extends HTMLElement {
     }
 
     _handleAccordionClick (ev) {
+        // Prevent the dropdown from opening on add
         if (ev.target === this.add_button) {
             return;
         }
 
-        if (this.panel.style.display === 'flex') {
-            this.panel.style.display = 'none';
-            this.accordion_icon.innerHTML = '&#8964;';
-        } else {
-            this.panel.style.display = 'flex';
-            this.accordion_icon.innerHTML = '&#8963;';
-        }
-    }
-    /**
-     * Populate the content of the spell item
-     */
-    populateSpellItem () {
-        this.spellName = this.spell.name;
-        this.spellDesc = this.spell.desc;
+        const panelStyle = this.elements.panel.style;
 
-        this.spellComps = this.spell.components;
-        this.spellDur = this.spell.duration;
-        this.spellCasting = this.spell.casting_time;
-        this.spellRange = this.spell.range;
-        this.spellConcentration = this.spell.concentration;
-
-        if (this.spell.higher_level !== undefined) {
-            this.spellHl = this.spell.higher_level;
-        }
-    }
-    /**
-     * Getter: Content of spell name
-     * @returns {String}
-     */
-    get spellName () {
-        return this.accordion.querySelector('.spell-item-name').innerHTML;
-    }
-    /**
-     * Setter: Content of spell name
-     */
-    set spellName (value) {
-        this.accordion.querySelector('.spell-item-name').innerHTML = value;
-    }
-    /**
-     * Getter: Content of spell's higher-level-casting
-     * @returns {String}
-     */
-    get spellHl () {
-        return this.panel.querySelector('.spell-item-hl').innerHTML;
-    }
-    /**
-     * Setter: Content of spell's higher-level-casting
-     */
-    set spellHl (value) {
-        this.panel.querySelector('.spell-item-hl').innerHTML = `<strong>At Higher Levels:</strong> `;
-
-        value.forEach((val) => {
-            const p = document.createElement('p');
-            p.innerHTML = val;
-
-            this.panel.querySelector('.spell-item-hl').appendChild(p);
-        });
-    }
-    /**
-     * Getter: Content of spell description
-     * @returns {String}
-     */
-    get spellDesc () {
-        return this.panel.querySelector('.spell-item-desc').innerHTML;
-    }
-    /**
-     * Setter: Content of spell description
-     */
-    set spellDesc (value) {
-        value.forEach((val) => {
-            const p = document.createElement('p');
-            p.innerHTML = val;
-
-            this.panel.querySelector('.spell-item-desc').appendChild(p);
-        });
-    }
-    /**
-     * Getter: Content of spell range
-     * @returns {String}
-     */
-    get spellRange () {
-        return this.panel.querySelector('.spell-item-range').innerHTML;
-    }
-    /**
-     * Setter: Content of spell range
-     */
-    set spellRange (value) {
-        value = `<strong>Range:</strong> ${value}`;
-
-        this.panel.querySelector('.spell-item-range').innerHTML = value;
-    }
-    /**
-     * Getter: Content of the spell's components
-     * @returns {String}
-     */
-    get spellComps () {
-        return this.stats.querySelector('.spell-item-comps').innerHTML;
-    }
-    /**
-     * Setter: Content of the spell's components
-     */
-    set spellComps (value) {
-        this.stats.querySelector('.spell-item-comps').innerHTML = `<strong>Components:</strong> `;
-
-        value.forEach((val) => {
-            const span = document.createElement('span');
-            span.innerHTML = val;
-
-            this.stats.querySelector('.spell-item-comps').appendChild(span);
-        });
-    }
-    /**
-     * Getter: Content of the spell duration
-     * @returns {String}
-     */
-    get spellDur () {
-        return this.stats.querySelector('.spell-item-dur').innerHTML;
-    }
-    /**
-     * Setter: Content of the spell duration
-     */
-    set spellDur (value) {
-        value = `<strong>Duration:</strong> ${value}`;
-
-        this.stats.querySelector('.spell-item-dur').innerHTML = value;
-    }
-    /**
-     * Getter: Content of the spell casting time
-     * @returns {String}
-     */
-    get spellCasting () {
-        return this.stats.querySelector('.spell-item-casting').innerHTML;
-    }
-    /**
-     * Setter: Content of the spell casting time
-     */
-    set spellCasting (value) {
-        value = `<strong>Casting time:</strong> ${value}`;
-
-        this.stats.querySelector('.spell-item-casting').innerHTML = value;
-    }
-    /**
-     * Getter: Content of the spell concentration
-     */
-    get spellConcentration () {
-        return this.stats.querySelector('.spell-item-conc').innerHTML;
-    }
-    /**
-     * Setter: Content of the spell concentration
-     */
-    set spellConcentration (value) {
-        value = `<strong>Concentration:</strong> ${value}`;
-
-        this.stats.querySelector('.spell-item-conc').innerHTML = value;
+        panelStyle.display = panelStyle.display === 'flex' ? 'none' : 'flex';
+        this.elements.accordion_icon.innerHTML = panelStyle.display === 'flex' ? '&#8964;' : '&#8963;'; // down chevron and up chevron
     }
 }
 
